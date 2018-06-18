0fb05c98663b0b46025269b146fcd61a22046dab
mofumofu3n <mofumofu3n@gmail.com>
2018-06-19 00:11:32 +0900

    Split handler class from server class

---
+++ src/revealcr/handler.cr
@@ -0,0 +1,53 @@
+require "http/server"
+
+module Revealcr
+  class Handler
+    include HTTP::Handler
+
+    def initialize(@file : String)
+    end
+
+    def call(context)
+      original_path = context.request.path.not_nil!
+      is_dir_path = original_path.ends_with? "/"
+
+      case original_path
+      when "/"
+        context.response.content_type = "text/html"
+        context.response.print Render.new(@file).to_s
+      when /^\/revealjs\//
+        file_path = "assets#{original_path}"
+        context.response.content_type = self.mime_type(file_path)
+        File.open(file_path) do |file|
+          IO.copy(file, context.response)
+        end
+      else
+        file_path = File.expand_path(original_path[1..-1])
+        if File.exists?(file_path)
+          context.response.content_type = self.mime_type(file_path)
+          File.open(file_path) do |file|
+            IO.copy(file, context.response)
+          end
+        else
+          context.response.status_code = 404
+          call_next(context)
+        end
+      end
+    end
+
+    private def mime_type(path)
+      case path
+      when /\.jpg$/
+        "image/jpeg"
+      when /\.png$/
+        "image/png"
+      when /\.js$/
+        "application/javascript"
+      when /\.css$/
+        "text/css"
+      else
+        "text/plain"
+      end
+    end
+  end
+end
